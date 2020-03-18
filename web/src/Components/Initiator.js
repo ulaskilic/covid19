03b5f6e92ae3be2c4b43645bf3b9633451d9a845
Ulas Kilic <ulash.klc@gmail.com>
2020-03-18 07:35:31 +0300

    google analytics router implementations

--- web/src/Components/Initiator.js
+++ web/src/Components/Initiator.js
@@ -1,14 +1,17 @@
 import React, {useEffect} from "react";
 import withStore from "../Contexts/GlobalStore/withStore";
 import {Backdrop, CircularProgress} from '@material-ui/core'
-import {api} from "../Services/Api";
-
+import {useHistory} from 'react-router-dom';
 const Initiator = props => {
     const {store, setStore} = props.context;
-
+    const history = useHistory();
     useEffect(() => {
         init()
     }, []);
+
+    useEffect(() => {
+        window.gtag('config', 'UA-160830716-1',{'page_path': history.location.pathname})
+    }, [history.location.pathname]);
 
     const init = async () => {
     };
